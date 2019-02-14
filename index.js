234d17f86c1fa1b529d731e8ec0d5b7895a9c2e3
V <v@Vs-MacBook-Pro.net.fpt>
2019-02-14 21:47:04 +0700

    them trang home

---
+++ index.js
@@ -0,0 +1,18 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.set("views", "views");
+app.set("views engine", "ejs");
+app.use(bodyParser.urlencoded());
+app.use(express.static('public'));
+
+// var router = require("./router/applicationRouter.js");
+// router.routing(app);
+
+app.get("/home", function (req, res) {
+    res.render("client/contentHomePage.ejs");
+});
+app.listen( 9999, function () {
+    console.log("Chay thanh cong");
+});
