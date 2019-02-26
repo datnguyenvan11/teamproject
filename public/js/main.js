07592ff6fb64538ca203690db0436e1190640964
John Smith <example@email.com>
2019-02-26 16:19:33 +0700

    sua

--- public/js/main.js
+++ public/js/main.js
@@ -125,6 +125,46 @@ AOS.init({
     //         document.body.style.backgroundColor = "pink";
     //     }
     // }
+
+    var scrollWindow = function() {
+        $(window).scroll(function(){
+            var $w = $(this),
+                st = $w.scrollTop(),
+                navbar = $('.ftco_navbar'),
+                sd = $('.js-scroll-wrap');
+
+            if (st > 150) {
+                if ( !navbar.hasClass('scrolled') ) {
+                    navbar.addClass('scrolled');
+                }
+            }
+            if (st < 150) {
+                if ( navbar.hasClass('scrolled') ) {
+                    navbar.removeClass('scrolled sleep');
+                }
+            }
+            if ( st > 350 ) {
+                if ( !navbar.hasClass('awake') ) {
+                    navbar.addClass('awake');
+                }
+
+                if(sd.length > 0) {
+                    sd.addClass('sleep');
+                }
+            }
+            if ( st < 350 ) {
+                if ( navbar.hasClass('awake') ) {
+                    navbar.removeClass('awake');
+                    navbar.addClass('sleep');
+                }
+                if(sd.length > 0) {
+                    sd.removeClass('sleep');
+                }
+            }
+        });
+    };
+    scrollWindow();
+
     //
     // $(window).resize(function(){
     //
@@ -135,48 +175,10 @@ AOS.init({
     //     }
     //     else{
     //
-    //         var scrollWindow = function() {
-    //             $(window).scroll(function(){
-    //                 var $w = $(this),
-    //                     st = $w.scrollTop(),
-    //                     navbar = $('.ftco_navbar'),
-    //                     sd = $('.js-scroll-wrap');
-    //
-    //                 if (st > 150) {
-    //                     if ( !navbar.hasClass('scrolled') ) {
-    //                         navbar.addClass('scrolled');
-    //                     }
-    //                 }
-    //                 if (st < 150) {
-    //                     if ( navbar.hasClass('scrolled') ) {
-    //                         navbar.removeClass('scrolled sleep');
-    //                     }
-    //                 }
-    //                 if ( st > 350 ) {
-    //                     if ( !navbar.hasClass('awake') ) {
-    //                         navbar.addClass('awake');
-    //                     }
-    //
-    //                     if(sd.length > 0) {
-    //                         sd.addClass('sleep');
-    //                     }
-    //                 }
-    //                 if ( st < 350 ) {
-    //                     if ( navbar.hasClass('awake') ) {
-    //                         navbar.removeClass('awake');
-    //                         navbar.addClass('sleep');
-    //                     }
-    //                     if(sd.length > 0) {
-    //                         sd.removeClass('sleep');
-    //                     }
-    //                 }
-    //             });
-    //         };
-    //         scrollWindow();
     //
     //     }
     // });
-
+    //
 
     var isMobile = {
         Android: function() {
@@ -458,7 +460,7 @@ if (width <= 600){
     });
 }
 else{
-   swiper = new Swiper('.swiper-container', {
+    swiper = new Swiper('.swiper-container', {
 
         slidesPerView: 4,
         spaceBetween: 30,
