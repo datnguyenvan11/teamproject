12cd1df3f8f355b1bc2d00c6f37286d9be99b174
John Smith <example@email.com>
2019-03-04 00:06:16 +0700

    sua5

--- public/js/validate.js
+++ public/js/validate.js
@@ -135,3 +135,245 @@ var validator1=$('#contactForm2').validate({
 
 
 });
+
+var validator2 = $('#formcreate').validate({
+    rules: {
+        tourName: {
+            required: true,
+
+
+        },
+        tourFId: {
+            required: true,
+
+        },
+        tourImage: {
+            required: true,
+        },
+        tourDayGo: {
+            required: true,
+        },
+        tourTotalDay: {
+            required: true,
+        },
+        tourPrice: {
+            required: true,
+        },
+        tourNumberOfSeats: {
+            required: true,
+        },
+        tourShortDescription: {
+            required: true,
+        },
+        tourType: {
+            required: true,
+        },
+        tourCategory: {
+            required: true,
+        },
+
+        tourDateGo: {
+            required: true,
+        },
+        tourDateGoCome: {
+            required: true,
+        },
+        tourGoFlight: {
+            required: true,
+        },
+        tourDateReturn: {
+            required: true,
+        },
+        tourDateReturnCome: {
+            required: true,
+        },
+        tourHotel: {
+            required: true,
+        },
+        tourGuideName: {
+            required: true,
+        },
+        tourGuideAddress: {
+            required: true,
+        },
+        tourGuideTel: {
+            required: true,
+        }, tourFocusDay: {
+            required: true,
+        }, tourConcentratedPlace: {
+            required: true,
+        },
+
+
+    },
+    messages: {
+        tourName: {
+            required: '**',
+        },
+        tourFId: {
+            required: '**',
+        },
+        tourImage: {
+            required: '**',
+        },
+        tourDayGo: {
+            required: '**',
+        },
+        tourTotalDay: {
+            required: '**',
+        },
+        tourPrice: {
+            required: '**',
+        },
+        tourNumberOfSeats: {
+            required: '**',
+        },
+        tourShortDescription: {
+            required: '**',
+        },
+        tourType: {
+            required: '**',
+        },
+        tourCategory: {
+            required: '**',
+        },
+
+        tourDateGo: {
+            required: '**',
+        },
+        tourDateGoCome: {
+            required: '**',
+        },
+        tourGoFlight: {
+            required: '**',
+        },
+        tourDateReturn: {
+            required: '**',
+        },
+        tourDateReturnCome: {
+            required: '**',
+        },
+        tourHotel: {
+            required: '**',
+        },
+        tourGuideName: {
+            required: '**',
+        },
+        tourGuideAddress: {
+            required: '**',
+        },
+        tourGuideTel: {
+            required: '**',
+        }, tourFocusDay: {
+            required: '**',
+        }, tourConcentratedPlace: {
+            required: '**',
+        },
+    },
+
+
+});
+
+var validator3 = $('#registerForm').validate({
+    rules: {
+        firstName: {
+            required: true,
+
+            maxlength: 100
+        },
+        email: {
+            required: true,
+            email: true
+        },
+        lastName: {
+            required: true,
+        },
+        password:{
+            required:true
+        },
+        confirmPassword: {
+            required: true,
+            equalTo: "#password"
+
+        },
+        dateOfBirth: {
+            required: true,
+        },
+        gender: {
+            required: true,
+        },
+        tell: {
+            required: true,
+        },
+        address: {
+            required: true,
+        },
+
+
+    },
+    messages: {
+        firstName: {
+            required: '*',
+
+            maxlength:'max 100'
+        },
+        email: {
+            required: '*',
+            email: true
+        },
+        lastName: {
+            required: '*',
+        },
+        password: {
+            required: '*',
+        },
+        confirmPassword: {
+            required: '*',
+            equalTo: "khong khop"
+
+        },
+        dateOfBirth: {
+            required: '*',
+        },
+        gender: {
+            required: '*',
+        },
+        tell: {
+            required: '*',
+        },
+        address: {
+            required: '*',
+        },
+
+    }
+
+});
+var validator4=$('#registerFormlogin').validate({
+    rules: {
+
+        email: {
+            required: true,
+            email: true
+        },
+        password: {
+            required: true,
+        },
+
+    },
+    messages: {
+
+
+        email: {
+            required: "* nhập email",
+            email: '* email đúng định dạng',
+
+        },
+
+        password: {
+            required: '* nhập password .',
+        }
+
+    },
+
+
+});
